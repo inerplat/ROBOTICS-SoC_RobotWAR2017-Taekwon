2c18859dc193bdf69bba926854d9df6f100defde
DH Kim <inerplat@gmail.com>
2017-08-02 15:08:32 +0900

    로봇 기초 탐지

--- MCU/img_process.c
+++ MCU/img_process.c
@@ -12,23 +12,19 @@
 #define RED 0
 #define GREEN 1
 #define BLUE 2
-void MCU_process(U16* buf, unsigned char image[], unsigned char gray[], int histcnt)
+int MCU_process(U16* buf, unsigned char image[], unsigned char gray[], int histcnt)
 {
   int i, j, cnt=0, k, l;
-  static int hiscnt=0;
-  int gaus3x3[3][3]={{1,2,1},{2,4,2},{1,2,1}};
-  unsigned char histogram[256];
   int gray_cnt=0;
   int sum;
-  int tmpR, tmpG, tmpB;
-  int sumR, sumG, sumB;
   double alpha, beta;
+  int first[180]={0}, last[180]={0}, density[180]={0};
+  int nodapBlack[180], LeftMax[180],RightMax[180];
   int hist[256];
   int Tmax;
   double VarMax;
-  double darkAvg = 0, brightAvg = 0;
-  int T = 5, darkCnt = 0, brightCnt = 0;
-
+  int maxi=0;
+int max;
   cnt=gray_cnt=0;
   memset(hist,0,256*sizeof(unsigned char));
   for(i=0;i<120;i++)
@@ -39,110 +35,90 @@ void MCU_process(U16* buf, unsigned char image[], unsigned char gray[], int hist
       image[cnt+GREEN]=(buf[gray_cnt]&0x07e0)>>3;
       image[cnt+BLUE]=(buf[gray_cnt]&0x001f)<<3;
       gray[gray_cnt]=(image[cnt+RED]+image[cnt+GREEN]+image[cnt+BLUE])/3;
+      if(gray[gray_cnt]>128) gray[gray_cnt]=255;
+      else
+      {
+        gray[gray_cnt]=0;
+        if(first[j]==0) first[j]=i;
+        last[j]=i;
+      }
       hist[gray[gray_cnt]]++;
       ++gray_cnt;
       cnt+=3;
     }
   }
-
-  if(!((histcnt)%10))
+  for(i=0;i<180;i++)
   {
-  sum = 0;
-  for (i = 0; i < 256; i++)
-  {
-    sum += histogram[i];
-    hist[i] = (int)((double)sum * 0.1181 + 0.5); // 누적합*((최대밝기/전체영상사이즈)+0.5[반올림])
-  }
-  gray_cnt=0;
-  for (i = 0; i < 120; i++)
-  {
-    for (j = 0; j < 180; j++)
+    for(j=115;j>=0;j--)
     {
-      gray[gray_cnt] = hist[gray[gray_cnt]];
-      gray_cnt++;
-    }
-  }
-}
-
-
-  VarMax = -1;
-
-  gray_cnt=0;
-  for (T = 120; T < 135; T++)
-  {
-  	darkCnt = brightCnt = darkAvg = brightAvg = 0;
-  	for (j = 0; j < 120; j+=2)
-  	{
-  		for (i = 0; i < 180; i+=2)
-  		{
-  			if (gray[gray_cnt] < T)
-  			{
-  				darkCnt++;
-  				darkAvg += gray[j*180+i];
-  			}
-  			else
-  			{
-  				brightCnt++;
-  				brightAvg += gray[j*180+i];
-  			}
-        ++gray_cnt;
-  		}
-  	}
-  	darkAvg = darkAvg / (double)(!darkCnt ? 1 : darkCnt);
-  	brightAvg = brightAvg / (double)(!brightCnt ? 1 : brightCnt);
-  	alpha = ((double)brightCnt / (double)(60 * 90)) * (double)100;
-  	beta = ((double)darkCnt / (double)(60 * 90)) * (double)100;
-  	if (VarMax < alpha*beta*(darkAvg - brightAvg)*(darkAvg - brightAvg))
-  	{
-  		Tmax = T;
-  		VarMax = alpha*beta*(darkAvg - brightAvg)*(darkAvg - brightAvg);
-  	}
-  }
-  gray_cnt=0;
-  for(i=0;i<120;i++)
-  {
-    for(j=0;j<180;j++)
-    {
-      if(gray[gray_cnt]<Tmax) gray[gray_cnt]=0;
-      else gray[gray_cnt]=255;
-      ++gray_cnt;
-    }
-  }
-
-/*
-  cnt=0;
-  gray_cnt=0;
-  for(i=0;i<120;i++)
-  {
-    for(j=0;j<180;j++)
-    {
-      if(!image[cnt+RED] && !image[cnt+GREEN] && !image[cnt+BLUE])
-      { gray[gray_cnt]=255;}
-      else gray[gray_cnt]=0;
-      ++gray_cnt;
-      cnt+=3;
-    }
-  }
-
-  gray_cnt=0;
-  for(i=3;i<117;i++)
-  {
-    for(j=3;j<177;j++)
-    {
-      if(gray[i*180+j])
+      if(gray[j*180+i]==0)
       {
-        for(k=1;k<=1;k++)
-        {
-          if(gray[(i-k)*180+j]<254 || gray[(i+k)*180+j]<254)
-          {
-            gray[i*180+j]=254;
-            break;
-          }
-        }
+        nodapBlack[i]=j;
+        buf[j*180+i]=2016;
+        break;
       }
     }
   }
-*/
+  max=0;
+
+LeftMax[0] = nodapBlack[0];
+  for(i = 1;i<180;i++)
+  {
+    if(nodapBlack[i] < LeftMax[i - 1])
+      LeftMax[i] = nodapBlack[i];
+    else
+      LeftMax[i] = LeftMax[i - 1];
+  }
+RightMax[179] = nodapBlack[179];
+for(i = 178;i>=0;i--)
+{
+  if(nodapBlack[i] < RightMax[i + 1])
+    RightMax[i] = nodapBlack[i];
+  else
+    RightMax[i] = RightMax[i + 1];
+
+    if(LeftMax[i] > RightMax[i])
+    {
+      if(max < nodapBlack[i] - LeftMax[i])
+      {
+        max = nodapBlack[i] - LeftMax[i];
+        maxi = i;
+      }
+    }
+    else
+    {
+      if(max < nodapBlack[i] - RightMax[i])
+      {
+        max = nodapBlack[i] - RightMax[i];
+        maxi = i;
+      }
+    }
+}
+
+for(j = 0;j<120;j++)
+buf[j*180+maxi]=0xf800;
+
+
+   /*
+   cnt=0;
+   gray_cnt=0;
+   for(i=0;i<120;i++)
+   {
+     for(j=0;j<180;j++)
+     {
+       buf[gray_cnt]=((gray[gray_cnt]>>3)<<11)+((gray[gray_cnt]>>2)<<5)+(gray[gray_cnt]>>3);
+       ++gray_cnt;
+       cnt+=3;
+     }
+   }
+   */
+   cnt=0;
+   for(j=0;j<120;j++)
+   {
+     buf[j*180+maxi]=63488;
+     if(gray[j*180+maxi]==0) ++cnt;
+   }
+/*
 cnt=0;
 gray_cnt=0;
 for(i=0;i<120;i++)
@@ -153,7 +129,8 @@ for(i=0;i<120;i++)
     ++gray_cnt;
     cnt+=3;
   }
-}
+*/
+return 1;
   //int R,G,B;
 /*
   cnt=0;
