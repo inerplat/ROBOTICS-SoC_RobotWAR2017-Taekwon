a6e9845f298ef9710073b98d5d22443fd614371d
Kim Sanggil <him3431@gmail.com>
2017-08-02 21:22:39 +0900

    로봇 공격모션, 회전모션 추가

--- MCU/robot_motion.c
+++ MCU/robot_motion.c
@@ -38,17 +38,26 @@ void Order_to_Robot(int motion_number)
 
   switch(motion_number)
   {
-	   case 2: printf("test_motion()"); test_motion();				break;
+	  case 2: printf("test_motion()"); test_motion();				break;
 	  case 2: printf("default_motion()"); default_motion();			break;
 	  case 4: printf("move_forward()"); move_forward();				break;
 	  case 5: printf("move_forwardx2()"); move_forwardx2();			break;
 	  case 6: printf("move_back()"); move_back();					break;
 	  case 8: printf("turn_left()"); turn_left();					break;
 	  case 9: printf("turn_right()"); turn_right();					break;
-	  case 11: printf("arm_attack_1()"); arm_attack_1();				break;
-	  case 12: printf("arm_attack_2()"); arm_attack_2();				break;
+	  case 11: printf("arm_attack_1()"); arm_attack_1();			break;
+	  case 12: printf("arm_attack_2()"); arm_attack_2();			break;
+	  case 13: printf("arm_attack_3()"); arm_attack_3();			break;
+	  case 15: printf("back_step_kick_1()"); back_step_kick_1();	break;
+	  case 17: printf("back_step_kick_2()"); back_step_kick_2();	break;
 	  case 21: printf("move_and_attack_1()"); move_and_attack_1();	break;
 	  case 22: printf("move_and_attack_2()"); move_and_attack_2();	break;
+	  case 23: printf("move_and_attack_3()"); move_and_attack_3();	break;
+	  case 24: printf("move_and_attack_4()"); move_and_attack_4();	break;
+	  case 34: printf("detail_turn_left()"); detail_turn_left();	break;
+	  case 35: printf("detail_turn_right()"); detail_turn_right();	break;
+	  case 37: printf("attack_combo_1()"); attack_combo_1();		break;
+	  case 41: printf("attack_combo_2()"); attack_combo_2();		break;
   }
 	uart_close();
   return;
